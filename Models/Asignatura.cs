65dd56ce83f642db8c8058cd14dd7bc65c09eae1
Tino3q <Hanlexgarcia31@gmail.com>
2026-01-24 16:05:35 -0400

    -Implementacion del modulo de Gestion de Asignaturas.

---
+++ Models/Asignatura.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionEstudiantes.Models;
+
+public class Asignatura
+{
+    [Key]
+    public int AsignaturaId { get; set; }
+
+    [Required(ErrorMessage = "El codigo es obligatorio.")]
+    public string? Codigo { get; set; }
+
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    public string? Nombre { get; set; }
+
+    [Required(ErrorMessage = "El aula es obligatoria.")]
+    public string? Aula { get; set; }
+
+    [Required(ErrorMessage = "Los creditos son obligatorios.")]
+    [Range(1, 10, ErrorMessage = "Los creditos deben estar entre 1 y 10.")]
+    public int Creditos { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
 builder.Services.AddDbContextFactory<Contexto>(options => options.UseSqlServer(ConStr));
 
 builder.Services.AddScoped<EstudiantesService>();
+builder.Services.AddScoped<AsignaturaService>();
 
 
 var app = builder.Build();
